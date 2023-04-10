925618919a673e32915a8926507ca347258f64df
Agnieszka Jach <jach.agnieszka@gmail.com>
2023-04-10 08:35:58 +0000

    add streamlit app

---
+++ dashboard/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import secured_app 
+import sb                       # refers to sidebar
+import home
+import sandbox
+import chat
+
+
+
+# Authentication check
+if not secured_app.check_password():
+    st.stop()
+# User authenticated, we can proceed
+
+# Load sidebar and pick page to render
+side_bar =  sb.sidebar_menu()
+
+if side_bar == "home":
+    home.run_app()
+
+elif side_bar == "sandbox":
+    sandbox.run_app()
+
+elif side_bar == "chat":
+    chat.run_app()
+
+else:
+    st.warning('not implemented yet')
